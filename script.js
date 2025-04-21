6a05ede46c557a1e3e3590f2c4e1fff0e855969d
Ardhana-Hidayat <ardhanahidayat61@gmail.com>
2025-04-21 12:33:25 +0700

    Add Dashboard

--- script.js
+++ script.js
@@ -16,9 +16,26 @@ fetch("https://ipinfo.io/json?token=8f8f1f3820adfb")
       <strong>Lokasi (koordinat):</strong> ${data.loc}
     `;
 
-    const [lat, lon] = data.loc.split(","); // Perbaikan di sini
+    const [lat, lon] = data.loc.split(",");
+    
     const mapUrl = `https://maps.google.com/maps?q=${lat},${lon}&z=15&output=embed`;
     document.getElementById("mapFrame").src = mapUrl;
+
+    fetch("https://script.google.com/macros/s/AKfycbxE-jKDj3LfqMNDAxout8CTCwTazIpdpWnYwRWdcc4iqSviEglxdA5F7YgDhkKTopWq/exec", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ip: data.ip,
+        city: data.city,
+        country: data.country,
+        loc: data.loc,
+        user_agent: navigator.userAgent,
+        platform: navigator.platform,
+        resolution: `${window.screen.width} x ${window.screen.height}`,
+      }),
+    });
   })
   .catch(() => {
     document.getElementById("ipInfo").innerHTML = "Gagal mengambil info IP.";
