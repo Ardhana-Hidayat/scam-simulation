a6f26d252310e3765e7168eab6d22f6f9859cb13
Ardhana-Hidayat <ardhanahidayat61@gmail.com>
2025-04-21 12:51:16 +0700

    Fix

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+// Ambil info dari browser
+const userAgent = navigator.userAgent;
+let platform = "Unknown";
+
+if (navigator.userAgentData && navigator.userAgentData.platform) {
+  platform = navigator.userAgentData.platform;
+} else {
+  platform = navigator.platform || "Unknown";
+}
+
 // Tampilkan info perangkat
 const deviceInfo = document.getElementById("deviceInfo");
 deviceInfo.innerHTML = `
-  <strong>User Agent:</strong> ${navigator.userAgent}<br>
-  <strong>Platform:</strong> ${navigator.platform}<br>
+  <strong>User Agent:</strong> ${userAgent}<br>
+  <strong>Platform:</strong> ${platform}<br>
   <strong>Resolusi Layar:</strong> ${window.screen.width} x ${window.screen.height}
 `;
 
+// Ambil data IP dan lokasi dari ipinfo
 fetch("https://ipinfo.io/json?token=8f8f1f3820adfb")
   .then((res) => res.json())
   .then((data) => {
@@ -17,10 +28,10 @@ fetch("https://ipinfo.io/json?token=8f8f1f3820adfb")
     `;
 
     const [lat, lon] = data.loc.split(",");
-    
     const mapUrl = `https://maps.google.com/maps?q=${lat},${lon}&z=15&output=embed`;
     document.getElementById("mapFrame").src = mapUrl;
 
+    // Kirim data ke Google Sheets via Apps Script
     fetch("https://script.google.com/macros/s/AKfycbxE-jKDj3LfqMNDAxout8CTCwTazIpdpWnYwRWdcc4iqSviEglxdA5F7YgDhkKTopWq/exec", {
       method: "POST",
       headers: {
@@ -31,8 +42,8 @@ fetch("https://ipinfo.io/json?token=8f8f1f3820adfb")
         city: data.city,
         country: data.country,
         loc: data.loc,
-        user_agent: navigator.userAgent,
-        platform: navigator.platform,
+        user_agent: userAgent,
+        platform: platform,
         resolution: `${window.screen.width} x ${window.screen.height}`,
       }),
     });
