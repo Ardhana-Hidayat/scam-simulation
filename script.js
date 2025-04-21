4186ee920a88e4a1fee18f2500be36243b3dd7ae
Ardhana-Hidayat <ardhanahidayat61@gmail.com>
2025-04-21 10:36:30 +0700

    Add Track Maps

--- script.js
+++ script.js
@@ -7,14 +7,18 @@ deviceInfo.innerHTML = `
 `;
 
 fetch("https://ipinfo.io/json?token=8f8f1f3820adfb")
-  .then(res => res.json())
-  .then(data => {
+  .then((res) => res.json())
+  .then((data) => {
     document.getElementById("ipInfo").innerHTML = `
       <strong>IP Address:</strong> ${data.ip}<br>
       <strong>Kota:</strong> ${data.city}<br>
       <strong>Negara:</strong> ${data.country}<br>
       <strong>Lokasi (koordinat):</strong> ${data.loc}
     `;
+
+    const [lat, lon] = data.loc.split(","); // Perbaikan di sini
+    const mapUrl = `https://maps.google.com/maps?q=${lat},${lon}&z=15&output=embed`;
+    document.getElementById("mapFrame").src = mapUrl;
   })
   .catch(() => {
     document.getElementById("ipInfo").innerHTML = "Gagal mengambil info IP.";
